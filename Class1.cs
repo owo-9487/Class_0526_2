fc7575d996d78dc2fd6772738052f3708d4a0cb0
Administrator <Administrator@A316-61>
2026-05-26 16:01:07 +0800

    updata

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Person
+{
+    public Person()
+    {
+        Student s1 = new Student();
+        Console.WriteLine(s1.GetName());
+    }
+
+    public class Student
+    {
+        public string GetName()
+        {
+            return "Student";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Class_0526_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+
+        }
+    }
+}
